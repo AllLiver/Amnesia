4db23acc6d5ec6917114d519ac0f901c70438b1d
AllLiver <97857498+AllLiver@users.noreply.github.com>
2024-02-22 09:14:00 -0700

    No need for test class anymore

--- src/classes/test-class/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "test.h"
-#include <iostream>
-using namespace std;
-
-void Test::test() {
-    cout << "Hello World!" << endl;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <cstdlib>
-
-#include "classes/test-class/test.h"
-
 using namespace std;
 
 int main() {
-    Test t1;
-    t1.test();
-    return 1;
+    
 }
-
