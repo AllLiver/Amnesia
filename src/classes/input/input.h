273756feff4b5738bd06ff34e43cb15485007b11
AllLiver <97857498+AllLiver@users.noreply.github.com>
2024-02-22 10:30:18 -0700

    Not working, dont use right now

--- src/classes/input/input.h
+++ src/classes/input/input.h
@@ -1,8 +1,9 @@
 #include <cstdlib>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Input {
     public:
-        string input(string msg);
+        string input(string msg, vector<string> validArgs);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 #include "classes/input/input.h"
 using namespace std;
 
 int main() {
-    Input i;
-    string buf = i.input("Enter something to be printed");
-    cout << buf << endl;
+    cout << "Welcome to Amnesia. Let the adventure begin. \n\n Cold. That’s the first thing you feel. Oppressive cold. You look around at unfamiliar surroundings. You don’t know where you are. Actually…\n\n You don’t know \e[3mwho\e[23m you are either. If you did know, you’d probably have some clue to why you’re locked up in this dungeon. Weathered gray brick walls surround you on all sides. The only items in the room are a dimly burning torch on the wall and a rusty sword on the floor. Directly across from you is a sturdy looking wooden door. The only question you have left is…\n\n What do you do now?" << endl;
+    Input opt1;
+    vector<string> opts{"sword", "torch"};
+    opt1.input("Pick up the sword or torch?", opts);
 }
