34bea5a66cdd75fabfb9ff0a88897cf0f6009240
Emil-Gruenwald <gruenwald.emil@gmail.com>
2024-03-05 10:30:13 -0700

    dx

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
-clear
 rm amnesia
+clear
 g++ -std=c++11 -o amnesia main.cpp classes/input/input.cpp classes/enemy/enemy.cpp classes/level/level.cpp classes/player/player.cpp classes/saves/saves.cpp classes/shield/shield.cpp classes/weapon/weapon.cpp classes/img/img.cpp
--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
-clear
 rm a.out
+clear
 g++ -std=c++11 main.cpp classes/input/input.cpp classes/enemy/enemy.cpp classes/level/level.cpp classes/player/player.cpp classes/saves/saves.cpp classes/shield/shield.cpp classes/weapon/weapon.cpp classes/img/img.cpp
 ./a.out
