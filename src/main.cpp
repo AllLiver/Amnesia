9daf340c7e785fce2b4315f4261eebd4ee24c4fe
Emil-Gruenwald <gruenwald.emil@gmail.com>
2024-03-12 09:36:45 -0600

    I removed a space because it was annoying me

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main() {
     Player player;
-    cout << "Welcome to Amnesia. Let the adventure begin. \n\n Cold. That’s the first thing you feel. Oppressive cold. You look around at unfamiliar surroundings. You don’t know where you are. Actually…\n\n You don’t know \e[3mwho\e[23m you are either. If you did know, you’d probably have some clue to why you’re locked up in this dungeon. Weathered gray brick walls surround you on all sides. The only items in the room are a dimly burning torch on the wall and a rusty sword on the floor. Directly across from you is a sturdy looking wooden door. The only question you have left is…\n\n What do you do now?" << endl;
+    cout << "Welcome to Amnesia. Let the adventure begin. \n\n Cold. That’s the first thing you feel. Oppressive cold. You look around at unfamiliar surroundings. You don’t know where you are. Actually…\n\n You don’t know \e[3mwho\e[23m you are either. If you did know, you’d probably have some clue to why you’re locked up in this dungeon. Weathered gray brick walls surround you on all sides. The only items in the room are a dimly burning torch on the wall and a rusty sword on the floor. Directly across from you is a sturdy looking wooden door. The only question you have left is…\n\nWhat do you do now?" << endl;
     Input opt1;
     opt1.opts.push_back("sword");
     opt1.opts.push_back("torch");
